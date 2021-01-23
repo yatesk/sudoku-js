10c4f12a57b5c3859b9d36856402fec945450e1b
Yates <kevinmyates@gmail.com>
2021-01-23 06:30:31 -0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+var destination = document.getElementById("container");
+
+function App() {
+    const [gameId, setGameId] = useState(1);
+
+    return (
+        <div>
+            <h1>Sudoku</h1>
+            {/* <TicTacToe key={gameId} resetGame={() => setGameId(gameId + 1)}/> */}
+        </div>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    destination
+); 
